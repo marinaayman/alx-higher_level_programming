9ff19b99bd198c1df00d46a47b6539ff1a4c1a70
marinaayman <marinaaymankaram@gmail.com>
2024-06-18 13:35:36 -0700

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let firstArgument = process.argv[2];
-let toInteger = parseInt(firstArgument, 10);
+const toInteger = parseInt(firstArgument, 10);
 
 if (toInteger !== toInteger) console.log('Not a number');
 else console.log(`My number: ${toInteger}`);
